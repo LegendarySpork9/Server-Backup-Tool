fedc3368233e8eb0e80165964eeb36d8c9fb9810
LegendarySpork9 <tobyhunter1999@btopenworld.com>
2024-01-22 21:47:29 +0000

    Release 1.1.0
    Release for Issue #1 .

--- Server Backup Tool/Server Information.cs
+++ Server Backup Tool/Server Information.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright © - 17/01/2024 - Toby Hunter
+using System.Configuration;
+
 namespace Server_Backup_Tool
 {
     internal class Server_Information
     {
-        public static string FilePath = @"C:\Minecraft Server";
+        public static string FilePath = ConfigurationManager.AppSettings["ServerLocation"];
+        public static bool IsRunning = false;
     }
 }
