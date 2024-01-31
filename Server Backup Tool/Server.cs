5ae25773aaa15ff95d2ee44c41eac4d713b69386
LegendarySpork9 <tobyhunter1999@btopenworld.com>
2024-01-31 19:15:19 +0000

    Release 1.2.0
    Release for issue number #5.

--- Server Backup Tool/Server.cs
+++ Server Backup Tool/Server.cs
@@ -30,6 +30,7 @@ namespace Server_Backup_Tool
                 S.StartInfo.WorkingDirectory = Server_Information.FilePath;
                 S.Start();
                 S.BeginOutputReadLine();
+                Server_Information.IsRunning = true;
                 return "Completed";
             }
 
@@ -58,6 +59,8 @@ namespace Server_Backup_Tool
             S.StandardInput.WriteLine("stop");
             S.StandardInput.Flush();
 
+            Server_Information.IsRunning = false;
+
             return "Stopped";
         }
 
@@ -67,7 +70,13 @@ namespace Server_Backup_Tool
             {
                 if (!string.IsNullOrEmpty(e.Data))
                 {
-                    ServerLog.Info(e.Data);
+                    switch (e.Data)
+                    {
+                        case String when e.Data.Contains("/INFO]"): ServerLog.Info(e.Data); break;
+                        case String when e.Data.Contains("/WARN]"): ServerLog.Warn(e.Data); break;
+                        case String when e.Data.Contains("/ERROR]"): ServerLog.Error(e.Data); break;
+                        case String when e.Data.Contains("/DEBUG]"): ServerLog.Debug(e.Data); break;
+                    }
 
                     if (e.Data.Contains($"{Server_Information.FilePath}>PAUSE"))
                     {
