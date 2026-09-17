28b221a7c81ebc5e3b6f91573a2117348904e695
LegendarySpork9 <tobyhunter1999@btopenworld.com>
2026-09-17 14:06:22 +0100

    Test Fix
    Fixed a bug in the timer converter that was causing tests to fail.

--- Server Backup Tool/Converters/Time Converter.cs
+++ Server Backup Tool/Converters/Time Converter.cs
@@ -23,39 +23,29 @@ namespace ServerBackupTool.Converters
             DateTime triggerDateTime = DateTime.SpecifyKind(
                 currentTime.Date.Add(parsedTime),
                 DateTimeKind.Utc);
-            string elapsedTime = GetElapsedTime(
-                currentTime,
-                triggerDateTime,
-                triggerTime);
 
-            TimeSpan timerDuration = DateTime.SpecifyKind(
-                DateTime.Parse(elapsedTime),
-                DateTimeKind.Utc).Subtract(currentTime);
+            DateTime targetDateTime = GetElapsedTime(
+                currentTime,
+                triggerDateTime);
+
+            TimeSpan timerDuration = targetDateTime.Subtract(currentTime);
 
             return timerDuration;
         }
 
         /// <summary>
-        /// Returns the date and time the timer should be triggered.
+        /// Returns the UTC date and time the timer should next trigger.
         /// </summary>
-        private string GetElapsedTime(
+        private static DateTime GetElapsedTime(
             DateTime currentTime,
-            DateTime triggerDateTime,
-            string triggerTime)
+            DateTime triggerDateTime)
         {
-            string? elapsedTime;
-
             if (triggerDateTime <= currentTime)
             {
-                elapsedTime = $"{_Clock.UtcNow.AddDays(1):dd/MM/yyyy} {triggerTime}";
+                triggerDateTime = triggerDateTime.AddDays(1);
             }
 
-            else
-            {
-                elapsedTime = $"{_Clock.UtcNow:dd/MM/yyyy} {triggerTime}";
-            }
-
-            return elapsedTime;
+            return triggerDateTime;
         }
     }
 }
